7379e7a563b272c639b6cbd1e58642368fd3339d
Kaustav Chakraborty <kaustav.ron@gmail.com>
2016-08-29 20:11:26 +0530

    Kaustav - Implement a photo filter application with having color changes

--- PhotoFilter/ViewController.swift
+++ PhotoFilter/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var img: UIImageView!
+    // Create a place to render the filtered image
+    let context = CIContext(options: nil)
+    
+    @IBAction func applyFilter(sender: AnyObject) {
+        
+        // Create an image to filter
+        let inputImage = CIImage(image: img.image!)
+        
+        // Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        
+        // Apply a filter to the image
+        let filteredImage = inputImage!.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        // Render the filtered image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent)
+        
+        // Reflect the change back in the interface
+        img.image = UIImage(CGImage: renderedImage)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
